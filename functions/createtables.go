f6f2a8117a2e0be4d88af357e7d9b419ee447142
Ahmad Alali <aalali>
2023-11-08 18:38:33 +0300

    match user to get id understood

--- functions/createtables.go
+++ functions/createtables.go
@@ -13,7 +13,7 @@ database, err := sql.Open("sqlite3", "./forum.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	usersTable, err2 := database.Prepare("CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY, username TEXT, Password TEXT, email TEXT)")
+	usersTable, err2 := database.Prepare("CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY, username TEXT NOT NULL, Password TEXT NOT NULL, email TEXT)")
 	if err2 != nil {
 		log.Fatal(err2)
 	}
@@ -21,18 +21,27 @@ database, err := sql.Open("sqlite3", "./forum.db")
 	if err3 != nil {
 		log.Fatal(err3)
 	}
+	if username != "" && password != ""{
 	usersTable.Exec()
 	usersTable, _ = database.Prepare("INSERT INTO Users (username, Password) VALUES (?, ?)")
-	usersTable.Exec(username, password)
+	usersTable.Exec(username, password)}
 	fmt.Println("test2")
 	fmt.Println(title)
 	fmt.Println(post)
 	postsTable.Exec()
-	id, _ := database.Query("SELECT user_id FROM Users")
+	id, _ := database.Query("SELECT user_id, username FROM Users")
 	var user_id int
+	var useridfinal int
+	var username2 string
 	for id.Next(){
-	id.Scan(&user_id)}
+	id.Scan(&user_id, &username2)
+	fmt.Println(username2)
+	if username2 == "user"{
+useridfinal = user_id}}
 	postsTable, _ = database.Prepare("INSERT INTO Posts (Title, body, user_id) VALUES (?, ?, ?)")
-	postsTable.Exec(title, post, user_id)
+	postsTable.Exec(title, post, useridfinal)
+	fmt.Println(title)
+	fmt.Println(post)
+	fmt.Println(user_id)
 
 }
