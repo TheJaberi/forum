867a6a578953f693cdc1c8e0a273e6ee66a1aa76
Ahmad Alali <aalali>
2023-11-18 13:36:15 +0300

    added filter by category button

--- main/main.go
+++ main/main.go
@@ -21,6 +21,7 @@ func main() {
 	forumfunc.CreateTables() // create table creates the database and the tables for the project
 	http.HandleFunc("/createcategory", forum.HandlerCreateCategory)
 	http.HandleFunc("/postpage/", forum.HandlerPostPage)
+	http.HandleFunc("/filter/", forum.HandlerFilter)
 	http.HandleFunc("/register", forum.HandlerRegister) // HandlerRegister has function NewUser which adds the data for the user to the database
 	http.HandleFunc("/login", forum.HandlerLogin) // HandlerLogin checks if the user is registered, if so it adds his data to a Global variable
 	http.HandleFunc("/post", forum.HandlerPost)  // HandlerPost adds the data in the post to the database
