f3fc6ff5c60bb0487771264adb72b4f05e18ba50
Ahmad Alali <aalali>
2023-12-31 17:40:57 +0300

    fixed

--- functions/removeinteraction.go
+++ functions/removeinteraction.go
@@ -2,19 +2,13 @@ package forum
 
 import (
 	"context"
-	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 )
 
 func RemoveInteraction(post_id int, user_id int) {
-	Database, err := sql.Open("sqlite3", "./forum.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer Database.Close()
 	query := "DELETE FROM `Interaction` where post_id = ? AND user_id = ?"
-	_, err2 := Database.ExecContext(context.Background(), query, post_id, user_id)
+	_, err2 := DB.ExecContext(context.Background(), query, post_id, user_id)
 	if err2 != nil { // the post is added using the ExecContext along with the userid which is in the LoggedUser variable
 		log.Fatal(err2)
 	}
