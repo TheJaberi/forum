d926cc3f0eb5451717cd49f2528694c096cd49b4
Ahmad Alali <aalali>
2024-01-12 14:00:46 +0300

    comments almost there

--- webApp/handlers/handlercommentsinteraction.go
+++ webApp/handlers/handlercommentsinteraction.go
@@ -5,7 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
-	"os"
+
 	// "fmt"
 	forum "forum/functions"
 
@@ -37,33 +37,40 @@ func HandlerCommentsLikes(w http.ResponseWriter, req *http.Request) {
 	remComment_id, _ := strconv.Atoi(req.FormValue("removeInteraction"))  // remove interaction handles the data from like or dislike button if the user logged has already clicked on it
 	fmt.Println(remComment_id)
 	user_id := forum.LoggedUser.Userid
+	commentPos := 0
+	for i:=0;i<len(forum.AllData.Postpage.Comments);i++{
+		if forum.AllData.Postpage.Comments[i].Comment_id == addComment_id || forum.AllData.Postpage.Comments[i].Comment_id == remComment_id {
+			commentPos = i
+			break
+		}
+	}
 	if addComment_id > remComment_id { // which ever value is greater determines whether to add or remove
-		os.Exit(0)
-		if req.URL.Path == "/likecomment/" {
-			if !forum.AllData.Postpage.Comments[addComment_id-1].CommentUserDislike {
+		if req.URL.Path == "/commentlike/" {
+			if !forum.AllData.Postpage.Comments[commentPos].CommentUserDislike {
 				forum.InsertInteraction(forum.AllData.Postpage.PostID, user_id, 1, false, addComment_id) // insert adds the interaction to the database 1 is like 0 is dislike
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserlike = true                         // changes the comment like or dislike for the logged in user in the all comments var
+				forum.AllData.Postpage.Comments[commentPos].CommentUserlike = true                         // changes the comment like or dislike for the logged in user in the all comments var
 			} else {
 				forum.UpdateInteraction(forum.AllData.Postpage.PostID, user_id, 1, false, addComment_id) // update is used if a like has to be changed to a dislike or vice versa
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserlike = true
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserDislike = false
+				forum.AllData.Postpage.Comments[commentPos].CommentUserlike = true
+				forum.AllData.Postpage.Comments[commentPos].CommentUserDislike = false
 			}
 		} else {
-			if !forum.AllData.Postpage.Comments[addComment_id-1].CommentUserlike {
+			if !forum.AllData.Postpage.Comments[commentPos].CommentUserlike {
 				forum.InsertInteraction(forum.AllData.Postpage.PostID, user_id, 0, false, addComment_id)
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserDislike = true
+				forum.AllData.Postpage.Comments[commentPos].CommentUserDislike = true
 			} else {
 				forum.UpdateInteraction(forum.AllData.Postpage.PostID, user_id, 0, false, addComment_id)
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserDislike = true
-				forum.AllData.Postpage.Comments[addComment_id-1].CommentUserlike = false
+				forum.AllData.Postpage.Comments[commentPos].CommentUserDislike = true
+				forum.AllData.Postpage.Comments[commentPos].CommentUserlike = false
 			}
 		}
 	} else {
+		fmt.Println(11)
 		forum.RemoveInteraction(forum.AllData.Postpage.PostID, user_id, false, remComment_id) // remove is greater means there is already an interaction that needs to be removed
-		forum.AllData.Postpage.Comments[remComment_id-1].CommentUserlike = false
-		forum.AllData.Postpage.Comments[remComment_id-1].CommentUserDislike = false
+		forum.AllData.Postpage.Comments[commentPos].CommentUserlike = false
+		forum.AllData.Postpage.Comments[commentPos].CommentUserDislike = false
 	}
-	fmt.Println(11)
 	forum.AllData.Postpage.LoggedUser = true
+	fmt.Println(forum.AllData.Postpage)
 	t.ExecuteTemplate(w, "postpage.html", forum.AllData.Postpage)
 }
